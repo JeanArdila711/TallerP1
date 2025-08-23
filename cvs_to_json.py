d7003be9d439cc563b0e8d572bbad2c529f82a98
JeanArdila711 <jardilaa@eafit.edu.co>
2025-08-23 17:50:34 -0500

    Cargando la base de datos

---
+++ cvs_to_json.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import json
+
+# Leer archivo csv
+df = pd.read_csv(r'movies.csv')
+
+# Guardar DF como Json
+df.to_json('movies.json', orient='records')
+
+with open('movies.json', 'r') as file:
+    movies = json.load(file)
+
+for i in range(100):
+    movie = movies[i]
+    print(movie)
+    break
